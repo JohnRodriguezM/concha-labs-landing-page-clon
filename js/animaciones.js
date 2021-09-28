e8cab4c9ef21e1b4c9dd7bc1b4a3ba5834a8b5e0
John Jairo Rodriguez Medina <johnjairorodriguez384@gmail.com>
2021-09-28 00:13:40 -0500

    noches

---
+++ js/animaciones.js
@@ -0,0 +1,12 @@
+    window.sr = ScrollReveal();
+
+    sr.reveal('.Hear_clear-h2',{
+        duration:3000,
+        origin:'top',
+        distance:'-100px'
+    });
+    sr.reveal('.customize_h2',{
+        duration:12000,
+        origin:'top',
+        distance:'-100px'
+    });
