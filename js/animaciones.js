7ea9fd6abcc119387fd70337f7793ce6dfb8563b
John Jairo Rodriguez Medina <johnjairorodriguez384@gmail.com>
2021-09-28 22:27:15 -0500

    mobile

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,5 +1,12 @@
     window.sr = ScrollReveal();
 
+    //sr.reveal('.logo_concha',{
+       // rotate:{
+         //   x: 360,
+         //   y:180
+         //       }
+    //});
+
     sr.reveal('.Hear_clear-h2',{
         duration:3000,
         origin:'top',
@@ -10,3 +17,36 @@
         origin:'top',
         distance:'-100px'
     });
+    sr.reveal('.main_section2-div1',{
+        duration:7500,
+        //origin:'left',//quitar
+        //distance:'-100px'//quitar
+    });
+    sr.reveal('.main_section3',{
+        duration:3500,
+        //origin:'right',//quitar
+        //distance:'-100px'//quitar
+    });
+    sr.reveal('.main_section4',{
+        duration:3500,
+        //origin:'right',//quitar
+        //distance:'-100px'//quitar
+    });
+    sr.reveal('.main_section5',{
+        duration:3500,
+        origin:'left',//quitar
+        distance:'-100px'//quitar
+    });
+    sr.reveal('.main_section6',{
+        duration:3500,
+        origin:'right',//quitar
+        distance:'-100px'//quitar
+    });
+    sr.reveal('#rotate',{
+        duration:4000,
+        rotate:{
+             x: 360,
+            y:180
+                }
+        
+     });
