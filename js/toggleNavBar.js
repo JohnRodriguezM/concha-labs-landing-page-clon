360860c30bb47d300cc2c57e54985b7dc0c49a5e
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-04-22 23:28:43 -0500

    código minificado

--- js/toggleNavBar.js
+++ js/toggleNavBar.js
@@ -1,18 +1,1 @@
-const $main = document.querySelector('main');
-const $footer = document.querySelector('footer');
-const $btnNavBar = document.getElementById('btn-hamburger');
-const $container = document.getElementById('ul-container');
-
-$btnNavBar.addEventListener('click',()=>{
-    $btnNavBar.classList.toggle('is-active');
-})
-
-$btnNavBar.addEventListener('click', e => {
-    console.log(e.target)
-    $container.classList.toggle('invisible')
-    $container.classList.toggle('visible')
-    $main.classList.toggle('hidden')
-    $main.classList.toggle('active')
-    $footer.classList.toggle('hidden')
-    $footer.classList.toggle('active')
-})
+const $main=document.querySelector("main"),$footer=document.querySelector("footer"),$btnNavBar=document.getElementById("btn-hamburger"),$container=document.getElementById("ul-container");$btnNavBar.addEventListener("click",()=>{$btnNavBar.classList.toggle("is-active")}),$btnNavBar.addEventListener("click",e=>{console.log(e.target),$container.classList.toggle("invisible"),$container.classList.toggle("visible"),$main.classList.toggle("hidden"),$main.classList.toggle("active"),$footer.classList.toggle("hidden"),$footer.classList.toggle("active")});
